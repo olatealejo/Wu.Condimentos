20c79c9e83974a58354416b37fe35c181e161cc4
Geronimogomez09 <geronimogomezepet20@gmail.com>
2025-10-16 16:36:10 -0300

    Verificar si ya se guardó la preferencia de tema

--- main.js
+++ main.js
@@ -2,4 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const botonTema = document.getElementById("boton-tema");
     const body = document.body;
+/*Verificar si ya se guardo un tema*/ 
+    if (localStorage.getItem("tema") === "claro") {
+    body.classList.add("modo-claro");
+    botonTema.textContent = "🌙 Modo oscuro";
+}
 });
