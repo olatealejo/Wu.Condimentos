88a9a8f8fb29230428b779bffa8741be1b15243c
olatealejo <alejoolate83@gmail.com>
2025-10-27 17:19:37 -0300

    Se añadieron los listeners para los botones 'Añadir al carrito'

--- carrito.js
+++ carrito.js
@@ -8,3 +8,11 @@ const contadorElemento = document.querySelector(".carrito-conteo");
 if (contadorElemento) {
     contadorElemento.textContent = carritoContador;
 }
+
+const botonesCarrito = document.querySelectorAll('input[type="submit"][value="Añadir al carrito"]');
+
+botonesCarrito.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        e.preventDefault();
+    });
+});
