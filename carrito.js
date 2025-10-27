1ea86dabbf83de2d739e66ed11776c96df06cc33
olatealejo <alejoolate83@gmail.com>
2025-10-27 17:19:16 -0300

    Se implementó actualización visual del contador del carrito

--- carrito.js
+++ carrito.js
@@ -3,3 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 let carritoContador = parseInt(localStorage.getItem("carritoContador")) || 0;
+
+const contadorElemento = document.querySelector(".carrito-conteo");
+if (contadorElemento) {
+    contadorElemento.textContent = carritoContador;
+}
