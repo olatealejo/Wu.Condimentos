e5d54ab02e353cbf1876c565df6b5add02f755d3
olatealejo <alejoolate83@gmail.com>
2025-10-27 17:26:35 -0300

    Se cerró estructura del script y funciones del carrito

--- carrito.js
+++ carrito.js
@@ -1,6 +1,5 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-});
 
 let carritoContador = parseInt(localStorage.getItem("carritoContador")) || 0;
 
@@ -14,9 +13,7 @@ const botonesCarrito = document.querySelectorAll('input[type="submit"][value="AÃ
 botonesCarrito.forEach(boton => {
     boton.addEventListener("click", (e) => {
         e.preventDefault();
-    });
-});
-
+  
 const formulario = boton.closest("form");
 const inputCantidad = formulario.querySelector('input[type="number"]');
 const cantidad = parseInt(inputCantidad.value) || 0;
@@ -41,3 +38,6 @@ if (cantidad > 0) {
 } else {
     alert("Por favor, selecciona una cantidad mayor a 0");
 }
+});
+});
+});
